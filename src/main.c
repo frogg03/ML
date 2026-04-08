e27775f9c6fec8876b972277e209f47b3d98a3c2
frogg03 <marinha.miguel.mm@gmail.com>
2026-04-08 21:26:24 +0100

    Simple test in main

--- src/main.c
+++ src/main.c
@@ -4,7 +4,21 @@
 
 int main (void)
 {
-    printf("Hello World!\n");
+    Matrix a = mat_alloc(2, 3);
+    Matrix b = mat_alloc(3, 4);
+    Matrix c = mat_alloc(2, 4);
+
+    for (size_t i = 0; i < a.rows * a.cols; i++)
+        a.data[i] = i;
+
+    for (size_t i = 0; i < b.rows * b.cols; i++)
+        b.data[i] = 3 * i % 10;
+
+    mat_dot(c, a, b);
+
+    mat_print(a);
+    mat_print(b);
+    mat_print(c);
 
     return 0;
 }
